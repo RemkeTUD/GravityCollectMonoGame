ae069ce035aae3e78c438ce817f062b9f16e1e7f
Remke Laptop <remkealbrecht@gmail.com>
2018-02-24 14:13:47 +0100

    TextBoxen hinzugefügt

---
+++ Game2/Textbox/TextDialog.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    
+    public class TextDialog
+    {
+        public static bool isInDialog = false;
+        List<Textbox> textboxes;
+        static int currentID = -1;
+        public static TextDialog currentTextDialog;
+        static KeyboardState state, prevstate;
+        public TextDialog(List<Textbox> textboxes)
+        {
+            this.textboxes = textboxes;
+        }
+
+        public static void setUpDialog(TextDialog dialog)
+        {
+            currentTextDialog = dialog;
+            isInDialog = true;
+            currentID = 0;
+        }
+
+        public static void nextID()
+        {
+            isInDialog = true;
+            currentID++;
+            if(currentID >= currentTextDialog.textboxes.Count)
+            {
+                currentID = -1;
+                isInDialog = false;
+            }
+        }
+
+        public static void update()
+        {
+            state = Keyboard.GetState();
+            if(isInDialog && state.IsKeyDown(Keys.Space)&& prevstate.IsKeyUp(Keys.Space))
+            {
+                nextID();
+            }
+            prevstate = Keyboard.GetState();
+        }
+
+        public static void draw(SpriteBatch batch)
+        {
+            if(isInDialog)
+                currentTextDialog.textboxes[currentID].Draw(batch);
+            update();
+        }
+
+    }
+}
---
+++ Game2/Textbox/Textbox.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    public class Textbox
+    {
+        string text;
+        Texture2D textureTest;
+        Rectangle rect;
+
+        public static SpriteFont font;
+
+        public Textbox(string text)
+        {
+            this.text = text;
+            rect = new Rectangle(100, 500, 1400, 300);
+            textureTest = Game1.cManager.Load<Texture2D>("textfield");
+            if (font == null)
+                font = Game1.cManager.Load<SpriteFont>("font");
+        }
+
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textureTest, rect, Color.White);
+            spriteBatch.DrawString(font, text, rect.Location.ToVector2() + new Vector2(40,40), Color.Black);
+        }
+
+    }
+}
